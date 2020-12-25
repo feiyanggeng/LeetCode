e201e5355466e2e86d39628c5a81e1fc4f564466
feiyanggeng <1053221101@qq.com>
2020-12-25 16:38:51 +0800

    分发糖果

--- 135-F.js
+++ 135-F.js
@@ -1,30 +1,31 @@
 
-const input = [29, 51, 87, 87, 72, 12];
-// const input = [1, 3, 2, 2, 1];
+// const input = [29, 51, 87, 87, 72, 12];
+const input = [1, 3, 2, 2, 1];
 // const input = [1, 2, 3, 1, 0];
+// const input = [5, 4, 3, 2, 1, 0];
 
 var candy = function (ratings) { 
-  let curMaxIndex = -1, candys = new Array(ratings.length).fill(1)
-  for (let i = 0; i < ratings.length; i++) {
-    const prev = typeof ratings[i - 1] === "number" ? ratings[i - 1] : Infinity;
-    const next = typeof ratings[i + 1] === "number" ? ratings[i + 1] : Infinity;
-    if (ratings[i] > prev) {
-      candys[i] = (candys[i - 1] || 0) + 1;
-    } else if (ratings[i] > next) {
-      candys[i]++;
-    }
-    if (ratings[i] > next) {
-      candys[curMaxIndex] &&
-        candys[curMaxIndex] < candys[i] &&
-        candys[curMaxIndex]++;
-      curMaxIndex = i;
+  const n = ratings.length;
+  const left = new Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+    if (i > 0 && ratings[i] > ratings[i - 1]) {
+      left[i] = left[i - 1] + 1;
     } else {
-      curMaxIndex = -1;
+      left[i] = 1;
     }
-    console.log(candys);
   }
-  console.log(candys);
-  return candys.reduce((prev, next) => prev + next, 0 )
+
+  let right = 0,
+    ret = 0;
+  for (let i = n - 1; i > -1; i--) {
+    if (i < n - 1 && ratings[i] > ratings[i + 1]) {
+      right++;
+    } else {
+      right = 1;
+    }
+    ret += Math.max(left[i], right);
+  }
+  return ret;
 };
 
 console.log(candy(input));
